880d7e0a6b501b7e0433b52df718c9eeca0592f6
Geronimo Peregalli <geronimovalles9@gmail.com>
2024-11-25 23:29:33 -0300

    se solucionan errores en el codigo

--- AppGero/urls.py
+++ AppGero/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from AppGero import views
 
 urlpatterns = [
-    path('AppGero/inicio/', views.inicio ),
-    path('AppGero/articulo/', views.articulo),
-    path('AppGero/herramienta/', views.herramienta),
-    path('AppGero/usuario/', views.usuario),
+    path('AppGero/inicio/', views.inicio, name='inicio' ),
+    path('AppGero/articulo/', views.articulo, name='articulo'),
+    path('AppGero/herramienta/', views.herramienta, name='herramienta'),
+    path('AppGero/usuario/', views.usuario, name='usuario'),
 ]
