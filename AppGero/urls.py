9e8293e5b46c79df650d8a3b795db56c49419307
Geronimo Peregalli <geronimovalles9@gmail.com>
2024-11-25 22:38:07 -0300

    se solucionan errores en el codigo

--- AppGero/urls.py
+++ AppGero/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AppGero import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('inicio/', views.inicio, name='inicio'),
     path('articulo/', views.articulo, name='articulo'),
     path('herramienta/', views.herramienta, name='herramienta'),
     path('usuario/', views.usuario, name='usuario'),
--- AppGero/views.py
+++ AppGero/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def inicio(request):
-    return render(request, 'index.html')
+    return render(request, 'appgero/index.html')
 
 def usuario(request):
-    return render(request, 'usuario.html')
+    return render(request, 'appgero/usuario.html')
 
 def articulo(request):
-    return render(request, 'articulo.html')
+    return render(request, 'appgero/articulo.html')
 
 def herramienta(request):
-    return render(request, 'herramienta.html')
+    return render(request, 'appgero/herramienta.html')
