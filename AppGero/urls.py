db1d0dc8e77c5b5dc95a6292c7761a4690686557
Geronimo Peregalli <geronimovalles9@gmail.com>
2024-12-09 19:39:03 -0300

    se agregan funcionalidades y informacion a index.html

--- AppGero/urls.py
+++ AppGero/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from AppGero import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('AppGero/inicio/', views.inicio, name='inicio' ),
     path('articulos/', views.leerArticulos, name='lista_articulos'),
     path('tutoriales/', views.leerTutoriales, name='lista_tutoriales'),
